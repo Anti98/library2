59b438af7a5a37ab8cdd21088bc7e453b398872b
Andrey <andreyshalyap@gmail.com>
2022-12-19 21:46:09 +0300

    Перенесен класс AuthServiceUtill

--- src/main/java/com/example/library2/service/AuthService.java
+++ src/main/java/com/example/library2/service/AuthService.java
@@ -2,6 +2,7 @@ package com.example.library2.service;
 
 import com.example.library2.exception.BadStatusCode;
 import com.example.library2.model.dto.security.UserCredentials;
+import com.example.library2.util.AuthServiceUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.authorization.client.AuthzClient;
@@ -10,7 +11,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
@@ -27,8 +27,10 @@ public class AuthService {
     }
 
     public AccessTokenResponse tokenRefresh(String refresh) {
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(AuthServiceUtil.getMap(authzClient, refresh), httpHeaders);
-        ResponseEntity<AccessTokenResponse> response = restTemplate.postForEntity(AuthServiceUtil.getRefreshUrlString(authzClient), request, AccessTokenResponse.class);
+        MultiValueMap<String, String> headerMap = AuthServiceUtil.getMap(authzClient, refresh);
+        String refreshTokenUrl = AuthServiceUtil.getRefreshUrlString(authzClient);
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(headerMap, httpHeaders);
+        ResponseEntity<AccessTokenResponse> response = restTemplate.postForEntity(refreshTokenUrl, request, AccessTokenResponse.class);
         if (response.getStatusCode().is2xxSuccessful())
             if (response.getBody() != null) return response.getBody();
             else return new AccessTokenResponse();
@@ -36,30 +38,11 @@ public class AuthService {
     }
 
     public String logout(String refreshToken) {
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(AuthServiceUtil.getMap(authzClient, refreshToken), httpHeaders);
-        ResponseEntity<AccessTokenResponse> response = restTemplate.postForEntity(AuthServiceUtil.getLogOutUrlString(authzClient), request, AccessTokenResponse.class);
+        String logOutUrl = AuthServiceUtil.getLogOutUrlString(authzClient);
+        MultiValueMap<String, String> headerMap = AuthServiceUtil.getMap(authzClient, refreshToken);
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(headerMap, httpHeaders);
+        ResponseEntity<AccessTokenResponse> response = restTemplate.postForEntity(logOutUrl, request, AccessTokenResponse.class);
         if (response.getStatusCode().is2xxSuccessful()) return "Logout complete";
         throw new BadStatusCode("SMTH go wrong");
-    }
-
-    private static class AuthServiceUtil {
-        private static String getLogOutUrlString(AuthzClient authzClient) {
-            return authzClient.getConfiguration().getAuthServerUrl() + "/realms/" + authzClient.getConfiguration().getRealm() + "/protocol/openid-connect/logout";
-        }
-
-        private static String getRefreshUrlString(AuthzClient authzClient) {
-            return authzClient.getConfiguration().getAuthServerUrl() + "/realms/" + authzClient.getConfiguration().getRealm() + "/protocol/openid-connect/token";
-        }
-
-        private static MultiValueMap<String, String> getMap(AuthzClient authzClient, String refresh) {
-            String clientId = authzClient.getConfiguration().getResource();
-            String secret = String.valueOf(authzClient.getConfiguration().getCredentials().get("secret"));
-            MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-            map.add("grant_type", "refresh_token");
-            map.add("refresh_token", refresh);
-            map.add("client_id", clientId);
-            map.add("client_secret", secret);
-            return map;
-        }
     }
 }
